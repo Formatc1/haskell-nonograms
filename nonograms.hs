1bd901317e3c2dc39b5755f9c37a33443e6d6d31
adam <edek-150@o2.pl>
2016-01-17 22:28:31 +0100

    brute force

--- nonograms.hs
+++ nonograms.hs
@@ -24,8 +24,12 @@ fillSimpleBoxes _ _ _ = []
 itFits :: Int -> [Int] -> Bool
 itFits 0 [] = True
 itFits _ [] = False
-itFits 0 (field:fields) = if field > 0 then False else True
-itFits num (field:fields) = if field < 0 then False else itFits (num - 1) fields
+itFits 0 (field:fields) = if field > 0
+    then False
+    else True
+itFits num (field:fields) = if field < 0
+    then False
+    else itFits (num - 1) fields
 
 --leftFillSimpleBoxes :: Int -> Int -> [Int] -> [Int]
 --leftFillSimpleBoxes _ size [] = replicate size 0
@@ -41,13 +45,21 @@ itFits num (field:fields) = if field < 0 then False else itFits (num - 1) fields
 --checkSimpleBoxes x = if (fst x) == (snd x) && (fst x) > 0 then 1 else 0
 
 intersectionElem :: (Int, Int) -> Int
-intersectionElem x = if (fst x) > 0 && (snd x) > 0 && (fst x) == (snd x) then 1 else if (fst x) < 0 || (snd x) < 0 then -1 else 0
+intersectionElem x = if (fst x) > 0 && (snd x) > 0 && (fst x) == (snd x) 
+    then 1
+    else if (fst x) < 0 || (snd x) < 0
+        then -1
+        else 0
 
 --simpleBoxes :: Int -> [Int] -> [Int]
 --simpleBoxes size numbers = map checkSimpleBoxes (zip x (rightFillSimpleBoxes (maximum x) size (reverse numbers))) where x = (leftFillSimpleBoxes 2 size numbers)
 
 unionElem :: (Int, Int) -> Int
-unionElem x = if (fst x) > 0 || (snd x) > 0 then 1 else if (fst x) < 0 || (snd x) < 0 then -1 else 0
+unionElem x = if (fst x) > 0 || (snd x) > 0 
+    then 1
+    else if (fst x) < 0 || (snd x) < 0
+        then -1
+        else 0
 
 addSolutionList :: ((Int, Int) -> Int) -> ([Int], [Int]) -> [Int]
 addSolutionList f x = map f (zip (fst x) (snd x))
@@ -56,13 +68,36 @@ addSolution :: ((Int, Int) -> Int) -> [[Int]] -> [[Int]] -> [[Int]]
 addSolution f x y = map (addSolutionList f) (zip x y)
 
 showBoard :: [[Int]] -> [Char]
-showBoard board = (foldr (\a b -> a ++ "\n" ++ b) [] (map (map (\a -> if a > 0 then '█' else ' ')) board))
+showBoard board = (foldr (\a b -> a ++ "\n" ++ b) [] (map (map (\a -> if a > 0
+    then '█'
+    else ' ')) board))
 
 count :: [Int] -> Int
 count row = length (filter (> 0) row)
 
+checkRow :: ([Int], [Int]) -> Bool
+checkRow row = (fst row) == (removeZeroFromHead (foldr checkRowFoldFun [] (snd row)))
+
 checkAndCompleteRow :: ([Int], [Int]) -> [Int]
-checkAndCompleteRow row = if (sum (fst row)) == (count (snd row)) then map (\x -> if x > 0 then 1 else -1) (snd row) else snd row
+checkAndCompleteRow row = if checkRow row
+    then map (\x -> if x > 0
+        then 1
+        else -1) (snd row)
+    else snd row
+
+removeZeroFromHead :: [Int] -> [Int]
+removeZeroFromHead [] = []
+removeZeroFromHead (0:list) = list
+removeZeroFromHead list = list
+
+checkRowFoldFun :: Int -> [Int] -> [Int]
+checkRowFoldFun x []
+    | x > 0 = [1]
+    | otherwise = [0]
+checkRowFoldFun x (row@(actual:rest))
+    | x > 0 = (actual+1):rest
+    | actual == 0 = row
+    | otherwise = 0:row
 
 reversePair :: ([a], [a]) -> ([a], [a])
 reversePair (a, b) = (reverse a, reverse b)
@@ -86,13 +121,47 @@ stepSolution horizontal vertical board = do
     board3 <- return (addSolution unionElem board3 (transpose board4))
     if (countProgress board3 > countProgress board1)
         then stepSolution horizontal vertical board3
-        else board3
+        else if sum (map sum horizontal) == countProgress board3
+            then board3
+            --else findCorrect horizontal (bruteForceBoard vertical board3) 
+            else board3
 
+genBruteForceRow :: Int -> [Int] -> [Int] -> [[Int]]
+genBruteForceRow 0 [] row = [row]
+genBruteForceRow emptyCells [] row = [row ++ replicate emptyCells (-1)]
+genBruteForceRow 0 (number:numbers) row 
+    | row == take (length row) [-1,-1..] = genBruteForceRow 0 numbers (row ++ replicate number 1)
+    | otherwise = genBruteForceRow 0 numbers (row ++ ((-1):replicate number 1))
+genBruteForceRow emptyCells listNumbers@(number:numbers) row
+    | row == take (length row) [-1,-1..] = (genBruteForceRow emptyCells numbers (row ++ replicate number 1)) ++ (genBruteForceRow (emptyCells - 1) listNumbers (row ++ [-1]))
+    | otherwise = (genBruteForceRow emptyCells numbers (row ++ ((-1):replicate number 1))) ++ (genBruteForceRow (emptyCells-1) listNumbers (row ++ [-1]))
+
+--[0,0,1,1,1,1,1,1,1,1,0,0,-1,0,0]
+bruteForce :: ([Int], [Int]) -> [[Int]]
+bruteForce ([], row) = [replicate (length row) (-1)]
+bruteForce (listNumbers, row) = filter (checkIfCover row) (genBruteForceRow ((length row) - ((sum listNumbers) + (length listNumbers) - 1)) listNumbers [])
+
+-- checkIfCover computedPartial fromBruteForce
+checkIfCover :: [Int] -> [Int] -> Bool
+checkIfCover [] [] = True
+checkIfCover [] _ = False
+checkIfCover _ [] = False
+checkIfCover (x:xs) (y:ys) 
+    | x >= 0 && y > 0 = checkIfCover xs ys
+    | x <= 0 && y < 0 = checkIfCover xs ys
+    | otherwise = False
+
+bruteForceBoard :: [[Int]] -> [[Int]] -> [[[Int]]]
+bruteForceBoard horizontal board = sequence (map bruteForce (zip horizontal board))
+
+findCorrect :: [[Int]] -> [[[Int]]] -> [[Int]]
+findCorrect vertical boards = transpose (map snd (head (filter (\x -> foldl (&&) True (map checkRow x)) (map ((zip vertical).transpose) boards))))
 
 main = do
     -- read board parameters from file
-    files <- getArgs
-    x <- (>>=) (readFile (head files)) (return.splitBy '\n')
+    --files <- getArgs
+    --x <- (>>=) (readFile (head files)) (return.splitBy '\n')
+    x <- (>>=) (readFile "boards/10x15.txt") (return.splitBy '\n')
     x <- return (map (splitBy ' ') x)
     x <- return (map (map (read::String->Int)) x)
     size <- return (head x)
@@ -104,5 +173,15 @@ main = do
     board <- return (replicate height (replicate width 0))
 
     board <- return (stepSolution horizontal vertical board)
-    writeFile (head (tail files)) (showBoard board)
+
+    --writeFile (head (tail files)) (showBoard board)
+    writeFile "output.txt" (showBoard board)
     print board
+
+
+--main = do
+----    --writeFile "output.txt" (showBoard (genBruteForceRow 3 [5,1] []))
+--    print (findCorrect [[5, 3], [7, 4], [2, 2, 3], [15], [2, 2, 2], [15], [2, 2, 2], [3, 7], [4, 5], [3, 3]] (bruteForceBoard [[1, 1], [7], [9], [2, 1, 1, 3], [2, 1, 1, 2], [2, 1, 1, 2], [4, 1], [7], [6], [2, 1, 1, 3], [2, 1, 1, 3], [4, 1, 3], [8], [6], [1, 1]] [[-1,-1,-1,1,-1,1,-1,-1,-1,-1],[0,1,1,1,1,1,1,0,0,0],[0,1,1,1,1,1,1,1,1,0],[1,1,-1,1,-1,1,-1,1,1,1],[1,1,0,1,-1,1,-1,0,1,0],[1,1,0,1,-1,1,-1,0,1,0],[1,1,1,1,-1,1,-1,-1,-1,-1],[0,1,0,1,1,1,1,1,0,0],[-1,-1,-1,1,1,1,1,1,1,-1],[1,1,-1,1,-1,1,-1,1,1,1],[1,1,-1,1,-1,1,-1,1,1,1],[1,1,1,1,-1,1,-1,1,1,1],[0,1,1,1,1,1,1,1,0,0],[-1,-1,1,1,1,1,1,1,-1,-1],[-1,-1,-1,1,-1,1,-1,-1,-1,-1]]))
+--    --print (countProgress [[-1,-1,-1,1,-1,1,-1,-1,-1,-1],[0,1,1,1,1,1,1,0,0,0],[0,1,1,1,1,1,1,1,1,0],[1,1,-1,1,-1,1,-1,1,1,1],[1,1,0,1,-1,1,-1,0,1,0],[1,1,0,1,-1,1,-1,0,1,0],[1,1,1,1,-1,1,-1,-1,-1,-1],[0,1,0,1,1,1,1,1,0,0],[-1,-1,-1,1,1,1,1,1,1,-1],[1,1,-1,1,-1,1,-1,1,1,1],[1,1,-1,1,-1,1,-1,1,1,1],[1,1,1,1,-1,1,-1,1,1,1],[0,1,1,1,1,1,1,1,0,0],[-1,-1,1,1,1,1,1,1,-1,-1],[-1,-1,-1,1,-1,1,-1,-1,-1,-1]])
+--    --print (sum (map sum [[5, 3], [7, 4], [2, 2, 3], [15], [2, 2, 2], [15], [2, 2, 2], [3, 7], [4, 5], [3, 3]]))
+--    --print (checkIfCorrect [[1], [2]] [[[1,1], [2,2]], [[3,3], [4,4]]])
